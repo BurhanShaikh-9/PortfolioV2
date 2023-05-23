932ccf0b74a7112368da87d16dc47264b15a2455
Burhan Ali Shaikh <85751219+Nyx-95@users.noreply.github.com>
2023-05-23 12:23:00 +0500

    update

--- src/website/component/project.jsx
+++ src/website/component/project.jsx
@@ -3,11 +3,36 @@ import fmd from "../../assets/projects/headerLogo.png"
 import arrayShare from "../../assets/projects/logoHeaderBlack.png"
 import resourcePad from "../../assets/projects/logo.png"
 import walaa from "../../assets/projects/walaLogo2.png"
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import indusLogo from "../../assets/projects/IndusHospitalLogo.png"
+// import { Carousel } from 'react-responsive-carousel';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export const Project = () => {
-  
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Show 3 slides on larger screens
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200, // Adjust the breakpoint as needed
+        settings: {
+          slidesToShow: 2, // Show 2 slides on medium-sized screens
+        },
+      },
+      {
+        breakpoint: 850, // Adjust the breakpoint as needed
+        settings: {
+          slidesToShow: 1, // Show 1 slide on smaller screens
+        },
+      },
+    ],
+  };
 
   return (
     <React.Fragment >
@@ -19,84 +44,65 @@ export const Project = () => {
             </h1>
           </div>
           <div className="works">
-          
+
             <div className="wrapper">
-              <Carousel
-               showArrows={true}
-               infiniteLoop={true}
-               autoPlay={false}
-               interval={100}
-               showThumbs={false}
-               showStatus={false}
-               swipeable={true}
-               emulateTouch={true}
-               dynamicHeight={false}
-               centerMode={false}
-               centerSlidePercentage={100}
-               selectedItem={0}
-               axis="horizontal"
-               // Adjust the number of items shown here
-               // For example, to show 3 items at a time
-               // and 2 items on smaller screens, you can use:
-               // slidesToShow={3}
-               // responsive={[
-               //   {
-               //     breakpoint: 768,
-               //     settings: {
-               //       slidesToShow: 2,
-               //     },
-               //   },
-               // ]}
-               slidesToShow={3} // Show 3 items at a time
-               responsive={[
-                 {
-                   breakpoint: 768, // Breakpoint for smaller screens
-                   settings: {
-                     slidesToShow: 2, // Show 2 items at a time
-                   },
-                 },
-                 {
-                   breakpoint: 480, // Breakpoint for mobile devices
-                   settings: {
-                     slidesToShow: 1, // Show 1 item at a time
-                   },
-                 },
-               ]}
-              >
-                <div className="worksInner">
-                  <img src={fmd} alt="" />
-                  <div className="blackwindow">
-                    <h2>Find My Doctor</h2>
-                    <p>App to book doctor and lab can even buy Pharmacy and Insurance</p>
-                    <p>UI/UX and Api integration</p>
-                  </div>
+              <Slider className='reactSlickCarousel' {...settings}>
+                <div className='item'>
+                  <a className="worksInner" draggable="false" href='https://find-my-doctor-v2.vercel.app/'>
+                    <img src={fmd} alt="" />
+                    <div className="blackwindow">
+                      <h2>Find My Doctor</h2>
+                      <p>App to book doctor and lab can even buy Pharmacy and Insurance</p>
+                      <p>UI/UX and Api integration</p>
+                    </div>
+                  </a>
                 </div>
-                <div className="worksInner">
-                  <img src={arrayShare} alt="" />
-                  <div className="blackwindow">
-                    <h2>Array Share</h2>
-                    <p>An application to share file across localhost</p>
-                    <p>UI/UX and designing</p>
-                  </div>
+                
+                <div className='item'>
+                  <a className="worksInner" draggable="false" href='https://indushospital.org.uk/dev/'>
+                    <img src={indusLogo} alt="" />
+                    <div className="blackwindow">
+                      <h2>Indus Hospital</h2>
+                      <p>An online donation app for hospital</p>
+                      <p>UI/UX and designing</p>
+                    </div>
+                  </a>
                 </div>
-                <div className="worksInner">
-                  <img src={resourcePad} alt="" />
-                  <div className="blackwindow">
-                    <h2>ResourcePad</h2>
-                    <p>A canadian company Portfolio</p>
-                    <p>UI/UX and designing</p>
-                  </div>
+                
+                <div className='item'>
+                  <a className="worksInner" draggable="false" href='https://arrayshare.netlify.app/'>
+                    <img src={arrayShare} alt="" />
+                    <div className="blackwindow">
+                      <h2>Array Share</h2>
+                      <p>An application to share file across localhost</p>
+                      <p>UI/UX and designing</p>
+                    </div>
+                  </a>
                 </div>
-                <div className="worksInner">
-                  <img src={walaa} alt="" />
-                  <div className="blackwindow">
-                    <h2>Walaa</h2>
-                    <p>An omani coupon application</p>
-                    <p>UI/UX and designing</p>
-                  </div>
+                
+                <div className='item'>
+                  <a className="worksInner" draggable="false" href='https://www.resourcepad.ca/'>
+                    <img src={resourcePad} alt="" />
+                    <div className="blackwindow">
+                      <h2>ResourcePad</h2>
+                      <p>A canadian company Portfolio</p>
+                      <p>UI/UX and designing</p>
+                    </div>
+                  </a>
                 </div>
-              
-              </Carousel>
+                
+                <div className='item'>
+                  <a className="worksInner" draggable="false" href='https://vercel.com/nyx-95/walaa-partner'>
+                    <img src={walaa} alt="" />
+                    <div className="blackwindow">
+                      <h2>Walaa</h2>
+                      <p>An omani coupon application</p>
+                      <p>UI/UX and designing</p>
+                    </div>
+                  </a>
+                </div>
+
+              </Slider>
             </div>
 
           </div>
