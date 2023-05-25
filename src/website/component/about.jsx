622c8b71673e3e5a8a8e370d17eeeb2f824d4d69
Nyx-95 <shaikhshady6@gmail.com>
2023-05-26 00:04:54 +0500

    update

--- src/website/component/about.jsx
+++ src/website/component/about.jsx
@@ -2,18 +2,20 @@ import React, { useEffect } from 'react'
 import img1 from '../../assets/about1.jpg'
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { useInView } from 'react-intersection-observer';
 
 export const About = () => {
-
+  const [refLeft, inRefLeftPic] = useInView();
+  const [refRight, inRefRightPic] = useInView();
   return (
     <React.Fragment >
       <div className="container">
         <div className="aboutInner">
-          <div className="leftSection">
+        <div className={`leftSection ${inRefLeftPic ? 'fade-left-in' : 'fade-left-out'}`} style={{ transitionDelay: '0.5s' }} ref={refLeft}>
             <img src={img1} alt="" />
           </div>
           <div className="rightSection">
-            <div className="rightInner">
+            <div className={`rightInner ${inRefRightPic ? 'fade-in' : 'fade-out'}`} style={{ transitionDelay: '0.5s' }} ref={refRight}>
               <h1>About</h1>
               <p>
               I am a Software Engineer, who keeps looking for the opportunity to get better, to learn and to improve my skills.
--- src/website/component/welcome.jsx
+++ src/website/component/welcome.jsx
@@ -17,7 +17,7 @@ export const Welcome = () => {
       <div className="container">
         <div className="welcomeInner">
         <div className="inner">
-        <img className={`no ${inViewNoon ? 'fade-in' : 'fade-out'}`} style={{ transitionDelay: '1.5s' }} id="baId" src={noon} alt="" ref={refNoon} />
+            <img className={`no ${inViewNoon ? 'fade-in' : 'fade-out'}`} style={{ transitionDelay: '1.5s' }} id="baId" src={noon} alt="" ref={refNoon} />
             <img className={`ha ${inViewHa ? 'fade-in' : 'fade-out'}`} style={{ transitionDelay: '1s' }} id="haId" src={ha} alt="" ref={refHa} />
             <img  className={`ba ${inViewBa ? 'fade-in' : 'fade-out'}`} style={{ transitionDelay: '0.5s' }}  id="naId" src={Ba} alt="" ref={refBa} />
           </div>
